a38f7fdc217be65a9442f0f7d376033c5764fac7
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2022-09-18 18:43:53 +0300

    hex

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+# @1
+euros_count = 100
+dollars = euros_count * 1.25
+rubles = dollars *60
+print(dollars)
+print(rubles)
+
+# @2
+stark = 'Arya'
+print(f'Do you want to eat, {stark}?')
+print("Yes, I'm hungry, mom.")
+
+# @3
+one = 'Naharis'
+two = 'Mormont'
+three = 'Sand'
+print(one[2]+two[1]+three[3]+two[4]+two[2])
+
+# @4
+value = 'Hexlet'
+print(value[2:5])
